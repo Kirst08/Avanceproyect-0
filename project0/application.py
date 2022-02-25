97c0559dbc43eea1d4a9f1a6a577336545fe6876
Kirst08 <kirstenlopez86@gmail.com>
2022-02-24 22:46:35 -0600

    primer commit

--- project0/application.py
+++ project0/application.py
@@ -1,1 +1,12 @@
-import flask
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/", methods= ["POST","GET"])
+def home():
+    return render_template("introduccion.html")
+
+@app.route("/estudios", methods= ["POST","GET"])
+def Estudios():
+    return render_template("estudios.html")
